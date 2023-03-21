aff499a44af17df6bf56fcf28660518f2f8289d0
mic0430 <chulaphang.25@gmail.com>
2023-03-20 21:11:00 -0700

    View:

--- src/mF/fieldView.java
+++ src/mF/fieldView.java
@@ -1,10 +1,20 @@
 package src.mF;
+
 import src.MVC.*;
+
+import javax.swing.*;
 import java.awt.*;
+import java.beans.PropertyChangeEvent;
 import java.util.Observable;
 
 public class fieldView extends View {
-    public fieldView(Model field) { super(field); }
+
+    public fieldView(Model field) {
+        super(field);
+
+    }
+
+    //display to user to the screen, display mineField (code) grid 2020
     public void paintComponent(Graphics gc) {
         super.paintComponent(gc);
         Color oldColor = gc.getColor();
@@ -13,4 +23,11 @@ public class fieldView extends View {
         shape.draw((Graphics2D) gc);
         gc.setColor(oldColor);
     }
+
+
+
+    public void propertyChange(PropertyChangeEvent evt) {
+        Model field = new Field();
+    }
 }
+
