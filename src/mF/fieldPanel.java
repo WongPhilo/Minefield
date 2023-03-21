ab2320babff24849a37f508f8d24eca57da97d6d
WongPhilo <philo.wong@sjsu.edu>
2023-03-21 13:02:49 -0700

    more trimming

--- src/mF/fieldPanel.java
+++ src/mF/fieldPanel.java
@@ -2,6 +2,8 @@ package src.mF;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.Border;
+
 import src.MVC.*;
 public class fieldPanel extends AppPanel {
     public fieldPanel(AppFactory factory) {
@@ -17,27 +19,28 @@ public class fieldPanel extends AppPanel {
         JButton S = new JButton("S");
         JButton SE = new JButton("SE");
 
-        controlPanel.add(NW);
-        controlPanel.add(N);
-        controlPanel.add(NE);
-        controlPanel.add(W);
-        controlPanel.add(filler);
-        controlPanel.add(E);
-        controlPanel.add(SW);
-        controlPanel.add(S);
-        controlPanel.add(SE);
-
-        NW.addActionListener(this);
-        N.addActionListener(this);
-        NE.addActionListener(this);
-        W.addActionListener(this);
-        E.addActionListener(this);
-        SW.addActionListener(this);
-        S.addActionListener(this);
-        SE.addActionListener(this);
+        buttonAdder(NW, this);
+        buttonAdder(N, this);
+        buttonAdder(NE, this);
+        buttonAdder(W, this);
+        JPanel jp = new JPanel();
+        jp.add(filler);
+        controlPanel.add(jp);
+        buttonAdder(E, this);
+        buttonAdder(SW, this);
+        buttonAdder(S, this);
+        buttonAdder(SE, this);
 
         controlPanel.setVisible(true);
         this.display();
+    }
+
+    public static void buttonAdder(JButton b, fieldPanel p) {
+        JPanel jp = new JPanel();
+        b.addActionListener(p);
+        jp.add(b);
+        p.add(jp);
+        p.setVisible(true);
     }
     public static void main(String args[]) {
         fieldPanel.run(new fieldFactory());
